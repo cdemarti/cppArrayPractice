1a399c3fdb4caa21c64bebd82fb227cb205adbd6
Cody DeMartin <codythomasdemartin@gmail.com>
2020-09-17 22:31:59 -0400

    init commit

---
+++ ArrayPractice/Source.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+// 	int myIntArray[10];
+// 
+// 	for (int i =0; i < 10; i++)
+// 	{
+// 		myIntArray[i] = i;
+// 		/*cout << myIntArray[i] << endl;*/
+// 	}
+// 
+// 	cout << "THe first element in array is: " << myIntArray[0] << endl;
+// 
+// 	
+// 
+// 	system("pause");
+
+	int myArray[5] = {1, 23, 5, 4, 9};
+
+	cout << "MyArray[0] =  " << myArray[0] << endl;
+	cout << "MyArray[1] =  " << myArray[1] << endl;
+	cout << "MyArray[2] =  " << myArray[2] << endl;
+	cout << "MyArray[3] =  " << myArray[3] << endl;
+	cout << "MyArray[4] =  " << myArray[4] << endl;
+
+	for (int i = 0; i < 5; i++)
+	{
+		cout << "myArray[" << i << "]";
+	}
+}
